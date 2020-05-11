afb6e6ea7a95ca12771e14d29670fd43405b8fa9
Piotr Kępiński <piotr.kepinski@totalmoney.pl>
2020-05-11 21:59:58 +0200

    esLint, prettier config

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat iusto debitis, ipsa
+        quisquam facilis quibusdam laudantium non? Facilis provident et repudiandae explicabo
+        voluptates adipisci laborum, neque error fugiat ipsa nobis sapiente quas. Rem fugit ab odit
+        velit iure explicabo consequuntur autem iste dolorem deleniti aliquam praesentium mollitia
+        sapiente minima nulla quia obcaecati molestias, illum provident expedita optio ipsa! Cum ut
+        repellendus magnam dolor illo asperiores aut amet, neque expedita! Ratione incidunt ipsam
+        inventore sequi eius facere similique quos placeat quis veniam, magnam debitis, architecto
+        itaque vel enim quas nulla delectus! Rem repellendus debitis a earum. Delect
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
